2f6ec88b1d6c8260d42522d60315b656669a902e
David Brooks <dave@bcs.co.nz>
2021-05-18 12:16:31 +1200

    Allow RDF to be extracted for a specific set of neurons in a model.

--- get_rdf.py
+++ get_rdf.py
@@ -7,13 +7,16 @@ import json
 import rdflib
 import requests
 
+from rdflib.namespace import RDF
+
 #===============================================================================
 
 SOMA_PROCESSES_URL = 'http://sparc-data.scicrunch.io:9000/scigraph/dynamic/demos/apinat/soma-processes.json'
 
 #===============================================================================
 
-APINATOMY_MODEL_BASE = 'https://apinatomy.org/uris/models/{}'
+APINATOMY_MODEL    = 'https://apinatomy.org/uris/models/{}'
+APINATOMY_MODEL_ID = 'https://apinatomy.org/uris/models/{}/ids/{}'
 
 #===============================================================================
 
@@ -46,6 +49,16 @@ def make_uri(name):
     else:
         return rdflib.Literal(name)
 
+
+#===============================================================================
+
+APINATOMY_LINK = make_uri('apinatomy:Link')
+
+PREDICATES_IGNORED = [
+    'apinatomy:rootOf',
+    'apinatomy:sourceOf',
+]
+
 #===============================================================================
 
 def get_json(url):
@@ -54,18 +67,36 @@ def get_json(url):
 
 #===============================================================================
 
-def get_rdf(model, query):
-    model_uri = APINATOMY_MODEL_BASE.format(model)
+def get_rdf_graph(model, neurons, query_data):
+    model_uri = APINATOMY_MODEL.format(model)
 
     graph = rdflib.Graph()
     for pfx, ns in namespaces.items():
         graph.bind(pfx, ns, override=True)
 
-    for edge in query.get('edges', []):
-        meta = edge.get('meta', {})
-        if ('Annotation' in meta.get('owlType', [])
-        and model_uri in meta.get('isDefinedBy', [])):
+    model_edges = [ edge for edge in query_data.get('edges', [])
+                if 'Annotation' in edge.get('meta', {}).get('owlType', [])
+                and model_uri in edge.get('meta', {}).get('isDefinedBy', []) ]
+
+    # Have a list of neurons we are interested in
+    # First go through edges building list of nodes connected to these neurons
+
+    if len(neurons) == 0:
+        connected_nodes = None
+    else:
+        connected_nodes = [APINATOMY_MODEL_ID.format(model, neuron) for neuron in neurons]
+        grown = True
+        while grown:
+            grown = False
+            for edge in model_edges:
+                if edge['sub'] in connected_nodes and edge['obj'] not in connected_nodes:
+                    connected_nodes.append(edge['obj'])
+                    grown = True
+
+    for edge in model_edges:
+        if connected_nodes is None or edge['sub'] in connected_nodes:
             graph.add( (make_uri(edge['sub']), make_uri(edge['pred']), make_uri(edge['obj'])) )
+
     return graph
 
 #===============================================================================
@@ -73,9 +104,11 @@ def get_rdf(model, query):
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description='Extract RDF about an ApiNATOMY model.')
+    parser = argparse.ArgumentParser(description='Save RDF about an ApiNATOMY model as Turtle.')
     parser.add_argument('--model', required=True,
-                        help='name of ApiNATOMY model')
+                        help='name of an ApiNATOMY model, e.g. ``keast-bladder``')
+    parser.add_argument('--neurons', nargs="+", default=[],
+                        help='restrict to specific neurons, e.g. ``snl59 snl69``')
     parser.add_argument('--source', metavar='JSON',
                         help='JSON file resulting from a SciCrunch query')
     args = parser.parse_args()
@@ -86,9 +119,10 @@ if __name__ == '__main__':
     else:
         process_model = get_json(SOMA_PROCESSES_URL)
 
-    rdf = get_rdf(args.model, process_model)
-    model_uri = APINATOMY_MODEL_BASE.format(args.model)
-    with open('{}.ttl'.format(args.model), 'w') as o:
+    rdf = get_rdf_graph(args.model, args.neurons, process_model)
+    model_uri = APINATOMY_MODEL.format(args.model)
+    with open('{}.ttl'.format(args.model) if len(args.neurons) == 0
+         else '{}_{}.ttl'.format(args.model, '_'.join(args.neurons)), 'w') as o:
         o.write(rdf.serialize(format='turtle', base=rdflib.URIRef(model_uri)).decode('utf-8'))
 
         # Don't set `base=map_uri` until RDFLib 5.0 and then use `explicit_base=True`
